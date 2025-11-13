068edc7bea581db2e86572c8e4f6b23579f45a7c
Nils Steinmetz <nils_steinmetz@web.de>
2025-11-13 14:20:11 +0100

    built prototype models for sqlalchemy

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from models import db, User, Projects, Content, Funding
 import os
 app = Flask(__name__)
 @app.route('/')
 def index():
+    pass
