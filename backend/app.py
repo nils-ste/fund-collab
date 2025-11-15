6f88fa23381037e9261e3d86cda16ba81de362c8
Nils Steinmetz <nils_steinmetz@web.de>
2025-11-15 14:56:25 +0100

    added flask routes

--- backend/app.py
+++ backend/app.py
@@ -22,3 +22,35 @@ def create_user():
     user = data_manager.create_user(request.form['username'], request.form['password'], request.form['email'], request.form['role'])
     return user
 
+@app.route('/users', methods=['PUT'])
+def update_user():
+
+@app.route('/users', methods=['DELETE'])
+def delete_user():
+
+@app.route('/project/<int:project_id>', methods=['GET'])
+def projects(project_id):
+
+@app.route('/project/<int:project_id>', methods=['POST'])
+def create_project(project_id):
+
+@app.route('/project/<int:project_id>', methods=['PUT'])
+def update_project(project_id):
+
+@app.route('/project/<int:project_id>', methods=['DELETE'])
+def delete_project(project_id):
+
+@app.route('/content/<int:content_id>', methods=['GET'])
+def content(content_id):
+
+@app.route('/content/<int:content_id>', methods=['POST'])
+def create_content(content_id):
+
+@app.route('/content/<int:content_id>', methods=['PUT'])
+def update_content(content_id):
+
+@app.route('/content/<int:content_id>', methods=['DELETE'])
+def delete_content(content_id):
+
+
+
