942c5c6b6a9f3dafeff613e74db29ea44ff9766c
Nils Steinmetz <nils_steinmetz@web.de>
2025-11-17 14:39:05 +0100

    continued building the backend

--- backend/routes/projects.py
+++ backend/routes/projects.py
@@ -2,18 +2,21 @@ from flask import request, jsonify, Blueprint
 from ..models import Projects, db
 bp = Blueprint('projects', __name__, url_prefix='/projects')
 
-@bp.route('/<int:project_id>', methods=['GET'])
-def projects(project_id):
-
+@bp.route('/<int:user_id>', methods=['GET'])
+def projects(user_id):
+    projects_user = Projects.query.filter_by(user_id=user_id).all()
+    if not projects_user:
+        return jsonify({})
+    return jsonify(projects_user)
     #retrieve json
 
-@bp.route('/<int:project_id>', methods=['POST'])
-def create_project(project_id):
+@bp.route('/<int:user_id>', methods=['POST'])
+def create_project(user_id):
     #request.json()
 
-@bp.route('/<int:project_id>', methods=['PUT'])
-def update_project(project_id):
+@bp.route('/<int:user_id>', methods=['PUT'])
+def update_project(user_id):
     #get id passed (query.get)
 
-@bp.route('/<int:project_id>', methods=['DELETE'])
-def delete_project(project_id):
+@bp.route('/<int:user_id>', methods=['DELETE'])
+def delete_project(user_id):
--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,19 +1,38 @@
 from flask import request, jsonify, Blueprint
-from ..models import Users
+from ..models import Users, db
 bp = Blueprint('users', __name__)
 
 @bp.route('/users', methods=['GET'])
-def user():
-    users = data_manager.get_users()
+def get_users():
+    users = Users.query.all()
+    if not users:
+        return jsonify({})
     return users
 
 @bp.route('/users', methods=['POST'])
-def create_user():
-    user = data_manager.create_user(request.form['username'], request.form['password'], request.form['email'], request.form['role'])
-    return user
+def create_user(user_json):
+    user_data = user_json.json()
+    db.session.add(user_data)
+    db.session.commit()
 
 @bp.route('/users', methods=['PUT'])
-def update_user():
+def update_user(user_id):
+    #fetch user by id
+    user = Users.query.get_or_404(user_id)
+    data = request.get_json()
+
+    #set editable fields (check if username and password should be edible this way)
+    editable_fields = ['email', 'password', 'role', 'project_ids']
+    #update selected fields
+    for field in editable_fields:
+        if field not in data:
+            setattr(user, field, data[field])
+
+    db.session.commit()
+    return user
+
 
 @bp.route('/users', methods=['DELETE'])
-def delete_user():
+def delete_user(user_id):
+    db.session.delete(Users.query.get(user_id))
+    db.session.commit()
