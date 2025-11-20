e1f07ec4a2ca7fe9ef20c28c8410f0190b5c795f
Nils Steinmetz <nils_steinmetz@web.de>
2025-11-20 15:28:42 +0100

    renamed routes components for easier handling

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,8 +1,8 @@
 def register_routes(app):
-    from .projects import bp as projects_bp
-    from .funding import bp as funding_bp
-    from .users import bp as users_bp
-    from .content import bp as content_bp
+    from .projects_router import bp as projects_bp
+    from .funding_router import bp as funding_bp
+    from .users_router import bp as users_bp
+    from .content_router import bp as content_bp
     app.register_blueprint(projects_bp)
     app.register_blueprint(funding_bp)
     app.register_blueprint(users_bp)
