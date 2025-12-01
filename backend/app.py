981bb33887d3edf8d4542a715e1ae2728f808fda
Nils Steinmetz <nils_steinmetz@web.de>
2025-12-01 12:16:45 +0100

    implemented cors

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request
+from flask_cors import CORS
 from models import db
 from routes import register_routes
 from config import Config
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db.init_app(app)
 register_routes(app)
+
+@app.route('/')
+def get_data():
+    return {"message": "Welcome to fund-collab!"}
+
 
 if __name__ == '__main__':
     with app.app_context():
