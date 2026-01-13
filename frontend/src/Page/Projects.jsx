1bcf05531d299559e2083589bfa9e27dcd04fc2a
Nils Steinmetz <nils_steinmetz@web.de>
2026-01-13 18:27:19 +0100

    minor bugfix in Projects

--- frontend/src/Page/Projects.jsx
+++ frontend/src/Page/Projects.jsx
@@ -68,8 +68,8 @@ export default function Projects({ userId }) {
           {project.status}
         </p>
 
-        <a
-          href={`/content/${project.id}`}
+        <Link
+          to={`/content/${project.id}`}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Show Project
@@ -88,7 +88,7 @@ export default function Projects({ userId }) {
               d="M1 5h12m0 0L9 1m4 4L9 9"
             />
           </svg>
-        </a>
+        </Link>
 
         {viewToggle === project.id && (
           <ProjectUpdateForm
