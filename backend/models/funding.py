d0c58b30e971d54edbac7b209b9a84517e207dcc
Nils Steinmetz <nils_steinmetz@web.de>
2026-02-03 14:50:03 +0100

    changes to funding model. added hyperlink and created_at, changed deadline format to date

--- backend/models/funding.py
+++ backend/models/funding.py
@@ -7,8 +7,10 @@ class Funding(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
     title = db.Column(db.String(80), nullable=False)
-    deadline = db.Column(db.String(80), nullable=False)
-    requirements = db.Column(db.String, nullable=False)
+    deadline = db.Column(db.Date, nullable=False)
+    hyperlink = db.Column(db.String(120), nullable=False)
+    requirements = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
 
     def to_dict(self):
-        return {"id":self.id, "title": self.title, "deadline": self.deadline,"requirements": self.requirements}
+        return {"id":self.id, "title": self.title, "deadline": self.deadline.isoformat(),"hyperlink": self.hyperlink ,"requirements": self.requirements}
