5d8e9c8cbba57f457b895495ca5f5e1ef243ff84
Nils Steinmetz <nils_steinmetz@web.de>
2026-02-28 20:42:17 +0100

    bugfixes with auth

--- backend/models/users.py
+++ backend/models/users.py
@@ -6,7 +6,7 @@ class Users(db.Model):
     Users table
     """
     id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
 
